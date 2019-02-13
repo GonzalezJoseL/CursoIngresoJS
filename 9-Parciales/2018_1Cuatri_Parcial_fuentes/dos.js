b46a3e657826031c1f3f175a4ef4dcfdcb5c8566
alumno <alumno>
2019-02-13 09:31:01 -0300

    asd

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,17 @@
 function mostrar()
 {
-  
+	var valor1;
+	var recargo1;
+	var resultadofinal;
+
+	valor1=prompt("Ingrese el valor del producto");
+	valor1=parseInt(valor1);
+
+	recargo1=(valor1*10)/100;
+	resultadofinal=(valor1+recargo1);
+	
+
+	alert("El precio del producto en efectivo es :$ "+valor1+" ,con tarjeta tiene un recargo del 10% que seria $ "+recargo1+" de recargo, costandole final de $ "+resultadofinal);
+
+
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -2,4 +2,36 @@
 function mostrar()
 {
 
+	var nombre1;
+	var nombre2;
+	var edad1;
+	var edad2;
+	var sumaedades;
+
+	
+	nombre1=prompt("Ingrese su nombre");
+	nombre2=prompt("Ingrese su nombre");
+
+	
+
+	edad1=prompt("Ingrese la edad del primer nombre");
+	edad1=parseInt(edad1);
+	edad2=prompt("Ingrese la edad del segundo nombre");
+	edad2=parseInt(edad2);
+	
+	sumaedades=(edad1+edad2);
+	
+	alert("Ustedes son "+nombre1+" y "+nombre2+" sus edades son "+edad1+ " y "+edad2+" ,y la suma de sus edades es "+sumaedades);
+
+
+
+
+
+
+
+
+
+
+
+
 }
