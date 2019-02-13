dca23b0a6af2a4da11eca3b9b7328118d6ecc627
alumno <alumno>
2019-02-13 11:33:40 -0300

    asd

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,45 @@
 function mostrar()
 {
 
+	var cantidadeproductos;
+	var precioproductos
+	var descuentodeldiez;
+	var descuentoadicional;
+	var pagocontarjeta;
+
+	cantidadeproductos=prompt("Ingrese cantidad de productos");
+	cantidadeproductos=parseInt(cantidadeproductos);
+
+	precioproductos=prompt("Ingrese el valor de los productos");
+	precioproductos=parseInt(precioproductos);
+
+	descuentodeldiez=((precioproductos*10)/100);
+	descuentoadicional=((precioproductos*25)/100);
+	
+
+	if (cantidadeproductos>2)
+		{
+			descuentodeldiez=((precioproductos*10)/100);
+			
+
+
+		}
+	else
+		{
+			
+		}
+
+	if (precioproductos>1999)
+
+		{	
+			descuentoadicional=((precioproductos*25)/100);
+		}
+
+
+
+
+
+
+
+
 }
